1224dbfe02e8ad68139968440ab4311c88db782c
= <stanbremer056@gmail.com>
2026-08-03 23:02:59 +0200

    fix windows

--- lib/src/platform.hpp
+++ lib/src/platform.hpp
@@ -171,7 +171,7 @@ inline auto set_send_timeout(SocketHandle const socket, std::int64_t millis) -> 
 }
 
 inline auto set_broadcast(SocketHandle const socket, bool enabled) -> std::int64_t {
-	BOOL value{enabled ? 1 : 0};
+	BOOL value = enabled ? 1u : 0u;
 	void const* erased = &value;
 	return ::setsockopt(socket, SOL_SOCKET, SO_BROADCAST, static_cast<char const*>(erased), sizeof(value));
 }
